ffe45681e4244512b7ec8c2a8aeae934bc08846d
Cameron McLaren <datacrunchercameron@gmail.com>
2023-03-14 21:17:29 +1100

    Initial commit

---
+++ lua/cams_folder/init.lua
@@ -0,0 +1,1 @@
+print("Hello from cams_folder/init.lua")
---
+++ lua/plugins.lua
@@ -0,0 +1,21 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim --
+ -- Only required if you have packer configured as `opt`
+ return require('packer').startup(function(use)
+   -- Packer can manage itself
+     use 'wbthomason/packer.nvim'
+--     use 'rstacruz/vim-closer'
+	print('Loading nvim tree')
+	use {
+	 	'nvim-tree/nvim-tree.lua',
+		requires = {
+			 	'nvim-tree/nvim-web-devicons', -- optional, for file icons
+			   },
+		tag = 'nightly' -- optional, updated every week. (see issue #1193)
+	  }
+	
+	use { 'nvim-tree/nvim-web-devicons'}
+	  
+	use {'neoclide/coc.nvim', branch = 'release'}
+
+	use {'benmills/vimux', config = function() vim.g.VimuxOrientation = 'v' end}
+end)
